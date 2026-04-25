eee7c6b3c1c050851d2ec8fd91aa4b93249d4587
Manbeer Singh <manbeersingh1300@gmail.com>
2026-04-25 20:49:03 +0530

    auth middleware jwt callback removed

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,35 +1,36 @@
-import jwt from 'jsonwebtoken'
-import type { JwtPayload, VerifyErrors } from 'jsonwebtoken'
-import type { Request, Response, NextFunction } from 'express';
+import env from "../config/env.js";
+import jwt from "jsonwebtoken";
+import type { JwtPayload } from "jsonwebtoken";
+import type { RequestHandler } from "express";
+import HttpError from "../utils/httpError.js";
 
 interface CustomJwtPayload extends JwtPayload {
-    id: string;
-    username: string;
+  id: string;
+  username: string;
 }
 
-const requireAuth = (req: Request, res: Response, next: NextFunction) => {
-    const authHeader = req.headers.authorization || (req.headers.Authorization as string | undefined);
-    
-    if (!authHeader?.startsWith('Bearer ')) {
-        return res.status(401).json({ message: 'Unauthorized: No token provided' });
-    }
-    
-    const token = authHeader.split(' ')[1]
+const requireAuth: RequestHandler = (req, res, next) => {
+  const authHeader =
+    req.headers.authorization ||
+    (req.headers.Authorization as string | undefined);
 
-    jwt.verify(
-        token,
-        process.env.ACCESS_TOKEN_SECRET!,
-        (err: VerifyErrors | null, decoded: string | JwtPayload | undefined) => {
-            if (err || !decoded) return res.status(403).json({ message: "Invalid or Expired token" })
-            
-            const payload = decoded as CustomJwtPayload;
-            
-            req.userId = payload.id;
-            req.username = payload.username;
-            
-            next();
-        }
-    )
-}
+  if (!authHeader?.startsWith("Bearer ")) {
+    throw new HttpError(401, "Unauthorized: No token provided");
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(
+      token,
+      env.ACCESS_TOKEN_SECRET,
+    ) as CustomJwtPayload;
+    req.userId = decoded.id;
+    req.username = decoded.username;
+    next();
+  } catch (err: any) {
+    throw new HttpError(403, "Invalid or Expired token");
+  }
+};
 
 export default requireAuth;
